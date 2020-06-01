2af7c638a1e5e23c4f7e9b45406667219a728e63
Haroldo Macêdo <Haroldo Macdo@Haroldo2017.mshome.net>
2020-06-01 14:21:30 +0100

    V2 of POST /employees/{id} Version 2 of POST /employee/{id}

--- VersionRefactoring/src/main/java/com/versioning/controller/employee/EmployeeController_Post.java
+++ VersionRefactoring/src/main/java/com/versioning/controller/employee/EmployeeController_Post.java
@@ -8,12 +8,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.versioning.entity.EmployeeV1;
+import com.versioning.entity.EmployeeV2;
 import com.versioning.entity.ErrorV1;
 import com.versioning.entity.PostId;
+import com.versioning.entity.PostIdV2;
 
 @RestController
 public class EmployeeController_Post {
 
+  //
+  //  Version 1
+  //
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   /**
    * POST /employees
    * V1
@@ -24,7 +30,52 @@ public class EmployeeController_Post {
   @PostMapping(path = "/employees", consumes = "application/nbs.si.v1+json")
   @ResponseStatus(HttpStatus.CREATED) // 201
   public @ResponseBody PostId postEmployeeV1(@RequestBody EmployeeV1 newEmployee) {
-    return new PostId(_postEmployeeV1(newEmployee));
+    //  Map to V2.
+    EmployeeV2 employeeV2 = new EmployeeV2();
+    employeeV2.setId(newEmployee.getId() + "");
+    employeeV2.setFirstName(firstName(newEmployee.getFullName()));
+    employeeV2.setLastName(lastName(newEmployee.getFullName()));
+    employeeV2.setPhone(newEmployee.getPhone());
+    employeeV2.setEmail(newEmployee.getEmail());
+    employeeV2.setStatus(newEmployee.getStatus());
+    
+    //  Transform the String to an Integer.
+    return new PostId(Integer.parseInt(_postEmployeeV2(employeeV2)));
+  }
+
+  /**
+   * Implements the approved way to transform a full name into a first name.
+   * @param fullName
+   * @return
+   */
+  private String firstName(String fullName) {
+    return fullName.substring(0, 5);
+  }
+
+  /**
+   * Implements the approved way to transform a full name into a last name.
+   * @param fullName
+   * @return
+   */
+  private String lastName(String fullName) {
+    return fullName.substring(5);
+  }
+
+  //
+  //  Version 2
+  //
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * POST /employees
+   * V2
+   * 
+   * @param newEmployee
+   * @return
+   */
+  @PostMapping(path = "/employees", consumes = "application/nbs.si.v2+json")
+  @ResponseStatus(HttpStatus.CREATED) // 201
+  public @ResponseBody PostIdV2 postEmployeeV2(@RequestBody EmployeeV2 newEmployee) {
+    return new PostIdV2(_postEmployeeV2(newEmployee));
   }
 
   /**
@@ -33,7 +84,7 @@ public class EmployeeController_Post {
    * @param employeeId
    * @return
    */
-  private int _postEmployeeV1(@RequestBody EmployeeV1 newEmployee) {
+  private String _postEmployeeV2(@RequestBody EmployeeV2 newEmployee) {
     return newEmployee.getId();
   }
 
